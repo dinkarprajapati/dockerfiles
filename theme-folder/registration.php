297b22d454971d92c2152d3d4bf898ec33c0d204
Dinkar Prajapati <dinkar.prajapati@webling.com.au>
2018-02-02 18:42:43 +1100

    removed the wrong folder uploaded in wrong place

--- theme-folder/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-    \Magento\Framework\Component\ComponentRegistrar::register(
-        \Magento\Framework\Component\ComponentRegistrar::THEME,
-        'frontend/Webling/marinucci',
-        __DIR__
-    );
